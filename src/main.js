84c503d5ce895a9b7e7adcbc9d67bcb30fabc722
Aleksandr Batollo <a@batollo.ru>
2020-08-05 11:59:24 +0300

    1.4 Пробуем отрендерить меню на страницу
    - Найдем контейнер siteTripMainElement для вставки меню
    - С помощью render отрисуем сгенерированную функцией createSiteMenuTemplate разметку в нужный контейнер.
    - Откроем public/index.html в браузере, чтобы проверить, что меню появилось на странице

--- src/main.js
+++ src/main.js
@@ -12,3 +12,9 @@ const createSiteMenuTemplate = () => {
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteTripMainElement = document.querySelector(`.trip-main`);
+const siteTripControlsElement = siteTripMainElement.querySelector(`.trip-controls`);
+const siteTripControlsSwitchElement = siteTripControlsElement.querySelector(`h2:first-of-type`);
+
+render(siteTripControlsSwitchElement, createSiteMenuTemplate(), `afterend`);
