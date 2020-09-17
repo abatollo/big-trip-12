83d8605f8b0c97329b3b5bcc17ce660bf6388599
Aleksandr Batollo <a@batollo.ru>
2020-09-17 06:05:52 +0300

    2.4 Описываем настройки сборщика
    - Установим режим разработки (development)
    - Укажем точку входа (entry) и выходные данные (output)
    - Включим карты исходников, чтобы было проще отлаживать код

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,5 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    watchContentBase: true,
-  }
+  devtool: 'source-map'
 };
